b9d4ca79342f0d4893d51ed9f612b4ab7d2674bc
Adam <adamllryan@gmail.com>
2024-02-23 20:17:56 -0500

    remap visual select surround insert pair

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -5,13 +5,6 @@ return {
 		"numToStr/Comment.nvim",
 		opts = {},
 	},
-	-- Move stuff with <M-j> and <M-k> in both normal and visual mode
-	{
-		"echasnovski/mini.move",
-		config = function()
-			require("mini.move").setup()
-		end,
-	},
 	-- Better buffer closing actions. Available via the buffers helper.
 	{
 		"kazhala/close-buffers.nvim",
@@ -20,5 +13,4 @@ return {
 		},
 	},
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	"tpope/vim-surround", -- Surround stuff with the ys-, cs-, ds- commands
 }
--- .config/nvim/lua/plugins/navbuddy.lua
+++ .config/nvim/lua/plugins/navbuddy.lua
@@ -1,19 +1,26 @@
+--[[
+
+  Navbuddy
+
+  Popup display that helps find and navigate to symbols in the current buffer.
+]]
+
 return {
-  "SmiteshP/nvim-navbuddy",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-    "SmiteshP/nvim-navic",
-    "MunifTanjim/nui.nvim",
-  },
-  event = "VeryLazy",
-  cmd = "Navbuddy",
-  keys = { { "<leader>sn", "<CMD>Navbuddy<CR>", desc = "Navbuddy" } },
-  config = function()
-    require("nvim-navbuddy").setup({
-      use_default_mappings = true,
-      lsp = {
-        preference = { "jedi_language_server" },
-      },
-    })
-  end,
+	"SmiteshP/nvim-navbuddy",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"SmiteshP/nvim-navic",
+		"MunifTanjim/nui.nvim",
+	},
+	event = "VeryLazy",
+	cmd = "Navbuddy",
+	keys = { { "<leader>sn", "<CMD>Navbuddy<CR>", desc = "Navbuddy" } },
+	config = function()
+		require("nvim-navbuddy").setup({
+			use_default_mappings = true,
+			lsp = {
+				preference = { "jedi_language_server" },
+			},
+		})
+	end,
 }
