3f7cb773612668e10fbc9889dd99753712962bd4
Adam <adamllryan@gmail.com>
2024-02-23 20:21:44 -0500

    added shortcut for insert visual selection delimiter

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -115,3 +115,7 @@ end, "Decrease scale factor")
 -- Map lm and to markdown
 
 map("n", "<leader>lm", "<cmd>MarkdownPreviewToggle<cr>", "Markdown Preview Toggle")
+
+-- Delimit selected
+
+map("v", "<C-i>", "ii", "Delimit selected with i")
