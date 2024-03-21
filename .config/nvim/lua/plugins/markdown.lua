a54a5ce53cb7b7253109561b0a1f106452a256f6
Adam <adamllryan@gmail.com>
2024-03-21 17:37:58 -0400

    replace headlines

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -7,10 +7,20 @@ return {
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
+	-- {
+	--
+	-- 	"lukas-reineke/headlines.nvim",
+	-- 	dependencies = "nvim-treesitter/nvim-treesitter",
+	-- 	config = true, -- or `opts = {}`
+	-- },
 	{
-
-		"lukas-reineke/headlines.nvim",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		config = true, -- or `opts = {}`
+		"MeanderingProgrammer/markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("markdown").setup({
+				headings = { "一", "二", "三", "四", "五", "六" },
+				bullet = "•",
+			})
+		end,
 	},
 }
